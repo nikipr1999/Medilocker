e4c777673bcf3915028d969533672e2e7a99ef18
Nikita Prajapati <nikiprajapati1999@gmail.com>
2020-02-11 12:34:29 +0530

    connection ho gaya

--- login_page/connect.php
+++ login_page/connect.php
@@ -5,12 +5,28 @@
  $dbname = "medilocker.sql";
  $conn = new mysqli($servername, $username, $password,$dbname) or die("Connect failed: %s\n". $conn -> error);
  
+
+$fullname=$_POST['name'];
+$email=$_POST['email'];
+$password=$_POST['pass'];
+
+
+
+
  if($conn)
  {
-  echo "done";
+  $stmt=$conn->prepare("insert into customer(fullname,email,password) values(?,?,?)");
+
+  $stmt->bind_param("sss", $fullname,$email,$password);
+  $stmt->execute();
+  
+  $stmt->close();
+  $conn->close();
 
  }
  else{
-  echo "connection failed";
+  alert('Sorry, unable to connect to the server.');
  }
+
+header('location: ../index.html');
  ?>
